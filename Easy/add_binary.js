3d3cd964826671e62e5d1397f6bc3dfda8d0920d
Wilfried Djoum Nguemtchue <wilfried.djoum@institutsaintjean.org>
2026-02-26 17:05:18 +0100

    feat: add binary

---
+++ Easy/add_binary.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function (a, b) {
+    let i = a.length - 1;
+    let j = b.length - 1;
+    let carry = 0;
+    let result = "";
+
+    while (i >= 0 || j >= 0 || carry) {
+        let sum = carry;
+
+        if (i >= 0) sum += a[i--] - '0';
+        if (j >= 0) sum += b[j--] - '0';
+
+        result = (sum % 2) + result;
+        carry = Math.floor(sum / 2);
+    }
+
+    return result;
+
+};
